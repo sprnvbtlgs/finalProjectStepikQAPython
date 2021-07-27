231aaf482bcf59656bce5b718b8090afa4c34ea2
virtuale <okmijn0001@ya.ru>
2021-07-27 22:33:54 +0300

    added handling exception and rewrite test

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.XPATH, "//a[@id='login_link']")
+        assert self.is_element_present(By.XPATH, "//a[@id='login_link']"), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,12 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 '''
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
